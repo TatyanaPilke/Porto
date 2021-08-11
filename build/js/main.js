f4d7cec2965718cf8e340f7c0e918ec6dc2835ad
TatyanaPilke <kykla.mc2@gmail.com>
2021-08-11 16:31:19 +0300

    project Porto

---
+++ build/js/main.js
@@ -0,0 +1,24 @@
+
+$(function () {
+
+  var mixer = mixitup('.works__container');
+
+  $('.reviews__slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
+});
+
+const iconMenu = document.querySelector('.menu__burger');
+const menuBody = document.querySelector('.menu__list');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
